c1a52794a120b3ed7382d75939b610ac22786697
Matt Herich <mjherich@gmail.com>
2020-04-04 17:22:48 -0700

    Switch to import syntax

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Import using commonJS require
-const gist = require('npm-gist-package');
+// const gist = require('npm-gist-package');
+
+// Switch to import syntax after adding "type": "module" to the package.json
+import gist from 'npm-gist-package';
 
 gist();
